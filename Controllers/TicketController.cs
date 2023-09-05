48011a2f6683b1463e7c631abd440de255a0eafe
KajetanBilinski <75189414+KajetanBilinski@users.noreply.github.com>
2023-09-05 23:08:05 +0200

    TicketController
    Added:
    Add, Update and Delete

--- Controllers/TicketController.cs
+++ Controllers/TicketController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PKS.Models.DBModels;
+using PKS.Models.DTO.Route;
 using PKS.Models.DTO.Stop;
 using PKS.Models.DTO.Ticket;
+using PKS.Services;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Sockets;
 
 namespace PKS.Controllers
@@ -13,10 +16,11 @@ namespace PKS.Controllers
     {
 
         public readonly PKSContext pks;
-        public TicketController(PKSContext pks)
+        private readonly IPKSModelValidator validator;
+        public TicketController(PKSContext pks, IPKSModelValidator pKSModelValidator)
         {
             this.pks = pks;
-            
+            this.validator = pKSModelValidator;
         }
         [HttpGet]
         public async Task<IActionResult> GetTickets()
@@ -63,5 +67,85 @@ namespace PKS.Controllers
             };
             return Ok(ticketReturn);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTicket(TicketAddDTO ticketAdd)
+        {
+            var error = validator.ValidateTicketAddDTO(ticketAdd);
+            if (error != null)
+            {
+                return BadRequest(error);
+            }
+            else
+            {
+                int idTicket = await pks.Ticket.CountAsync() > 0 ? await pks.Ticket.MaxAsync(t => t.idTicket) + 1 : 1;
+                var ticket = new Ticket()
+                {
+                    idTicket= idTicket,
+                    idBus = ticketAdd.idBus,
+                    idDiscount = ticketAdd.idDiscount,
+                    idRoute = ticketAdd.idRoute,
+                    idPassenger = ticketAdd.idPassenger,
+                    ValidFrom = ticketAdd.ValidFrom,
+                    ValidTo = ticketAdd.ValidTo,
+                    Validated = ticketAdd.Validated,
+                    SeatNumber = ticketAdd.SeatNumber
+                };
+                await pks.Ticket.AddAsync(ticket);
+                if (await pks.SaveChangesAsync() <= 0)
+                    return StatusCode(505);
+                else
+                    return Ok("Ticket added");
+            }
+        }
+
+        [HttpDelete("{idTicket}")]
+        public async Task<IActionResult> DeleteTicket(int idTicket)
+        {
+            if (!await pks.Ticket.AnyAsync(t => t.idTicket == idTicket))
+            {
+                return BadRequest($"Ticket with id: {idTicket} doesn't exist");
+            } 
+            else
+            {
+                var ticket = await pks.Ticket.FirstOrDefaultAsync(t => t.idTicket == idTicket);
+                pks.Ticket.Remove(ticket);
+                if (await pks.SaveChangesAsync() <= 0)
+                    return StatusCode(505);
+                else
+                    return Ok($"Ticket with id: {idTicket} was deleted");
+            }
+        }
+
+        [HttpPut("{idTicket}")]
+        public async Task<IActionResult> UpdateTicket(int idTicket, TicketAddDTO ticketUpdate)
+        {
+            var error = validator.ValidateTicketAddDTO(ticketUpdate);
+            if (error != null)
+            {
+                return BadRequest(error);
+            }
+            else if (!await pks.Ticket.AnyAsync(t => t.idTicket == idTicket))
+            {
+                return BadRequest($"Ticket with id: {idTicket} doesn't exist");
+            }
+            else
+            {
+                var ticket = await pks.Ticket.FirstOrDefaultAsync(t => t.idTicket == idTicket);
+                ticket.idBus = ticketUpdate.idBus;
+                ticket.idDiscount = ticketUpdate.idDiscount;
+                ticket.idRoute = ticketUpdate.idRoute;
+                ticket.idPassenger = ticketUpdate.idPassenger;
+                ticket.ValidFrom = ticketUpdate.ValidFrom;
+                ticket.ValidTo = ticketUpdate.ValidTo;
+                ticket.Validated = ticketUpdate.Validated;
+                ticket.SeatNumber = ticketUpdate.SeatNumber;
+                if (await pks.SaveChangesAsync() <= 0)
+                    return StatusCode(505);
+                else
+                    return Ok("Ticket updated");
+            }
+        }
+
     }
 }
---
+++ Models/DTO/Ticket/TicketAddDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace PKS.Models.DTO.Ticket
+{
+    public class TicketAddDTO
+    {
+        public decimal Cost { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public bool Validated { get; set; }
+        public string SeatNumber { get; set; }
+        public string RouteName { get; set; }
+        public decimal Distance { get; set; }
+        public string DiscountName { get; set; }
+        public int idBus { get; set; }
+        public int idRoute { get; set; }
+        public int idPassenger { get; set; }
+        public int idDiscount { get; set; }
+    }
+}
