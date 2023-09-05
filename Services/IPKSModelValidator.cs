7d03b431e03eac97ffc2cd84956fd46e58d8b228
KajetanBilinski <75189414+KajetanBilinski@users.noreply.github.com>
2023-09-05 22:53:13 +0200

    Updated validator
    Added method for validate stop

--- Services/IPKSModelValidator.cs
+++ Services/IPKSModelValidator.cs
@@ -6,6 +6,7 @@ using PKS.Models.DTO.BusType;
 using PKS.Models.DTO.Discount;
 using PKS.Models.DTO.Passenger;
 using PKS.Models.DTO.Route;
+using PKS.Models.DTO.Stop;
 
 namespace PKS.Services
 {
@@ -17,5 +18,6 @@ namespace PKS.Services
         public string? ValidateDiscountAddDTO(DiscountAddDTO discount);
         public string? ValidatePassengerAddDTO(PassengerAddDTO passenger);
         public string? ValidateRouteAddDTO(RouteAddDTO route);
+        public string? ValidateStopAddDTO(StopAddDTO stopAdd);
     }
 }
